3c13b8c0fc059c1fc9a65fe6ddd0a3248a99cde4
MightContainNuts <mightcontainnuts@icloud.com>
2025-01-23 13:09:24 +0100

    setup flask db

---
+++ book_alchemy.py
@@ -0,0 +1,11 @@
+from app import create_app,db
+from flask_migrate import Migrate
+
+app = create_app('development')
+migrate = Migrate(app, db)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 5000, host = '0.0.0.0')
